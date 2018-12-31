1157e34702c53f26f5b7821f5d6ad69f8c613549
Friedrich von Never <friedrich@fornever.me>
2018-12-31 19:44:09 +0700

    Initial commit

---
+++ ProcessWatcher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Management;
+
+namespace ProcessWatcher
+{
+    public static class Program
+    {
+        private static ManagementEventWatcher CreateWatcher()
+        {
+            var watcher = new ManagementEventWatcher(new WqlEventQuery("select * from Win32_ProcessStartTrace"));
+            watcher.EventArrived += (_, e) =>
+            {
+                Console.WriteLine($"Process: {e.NewEvent.Properties["ProcessName"].Value}");
+            };
+            return watcher;
+        }
+
+        public static void Main()
+        {
+            using (var watcher = CreateWatcher())
+            {
+                Console.WriteLine("Starting the watch. Press Enter to exit");
+                watcher.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+ProcessWatcher
+==============
+
+`ProcessWatcher` allows you to trace the processes started while it is running.
+
+Requires admin privileges on the default system settings (because it touches
+WMI).
