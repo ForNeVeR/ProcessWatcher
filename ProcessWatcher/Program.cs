8800a277f7978e7fef13c825dc043d3921016858
Friedrich von Never <friedrich@fornever.me>
2020-05-25 20:07:22 +0700

    Add process command line logging

--- ProcessWatcher/Program.cs
+++ ProcessWatcher/Program.cs
@@ -1,16 +1,37 @@
-﻿using System;
+﻿﻿using System;
 using System.Management;
 
 namespace ProcessWatcher
 {
     public static class Program
     {
+        private static string GetProcessCommandLine(uint processId)
+        {
+            var query = new WqlObjectQuery($"select CommandLine from Win32_Process where ProcessId = {processId}");
+            using (var searcher = new ManagementObjectSearcher(query))
+            {
+                using (var result = searcher.Get())
+                {
+                    foreach (var x in result)
+                    {
+                        return Convert.ToString(x["CommandLine"]);
+                    }
+                }
+            }
+
+            return "[NOT DETERMINED]";
+        }
+
         private static ManagementEventWatcher CreateWatcher()
         {
             var watcher = new ManagementEventWatcher(new WqlEventQuery("select * from Win32_ProcessStartTrace"));
             watcher.EventArrived += (_, e) =>
             {
-                Console.WriteLine($"Process: {e.NewEvent.Properties["ProcessName"].Value}");
+                var properties = e.NewEvent.Properties;
+                var processId = Convert.ToUInt32(properties["ProcessID"].Value);
+                var processName = Convert.ToString(properties["ProcessName"].Value);
+                var commandLine = GetProcessCommandLine(processId);
+                Console.WriteLine($"Process {processId}: {processName} {commandLine}");
             };
             return watcher;
         }
